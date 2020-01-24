fe6cad9ac86bea9f5709967484f68fa1650b2396
Andrew Adcock <andrewadcock@gmail.com>
2020-01-24 12:35:44 -0500

    Flesh out article listings

--- symfony/src/Controller/ArticleController.php
+++ symfony/src/Controller/ArticleController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,16 +17,34 @@ class ArticleController extends AbstractController
      * @Route("/", name="app_homepage")
      * @return Response
      */
-    public function homepage()
+    public function homepage(EntityManagerInterface $em)
     {
-        return $this->render('home.html.twig');
+        $repository = $em->getRepository(Article::class);
+        $articles = $repository->findAll();
+
+        return $this->render('home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     /**
      * @Route("/blog/{slug}")
      */
-    public function show($slug)
+    public function show($slug, EntityManagerInterface $em)
     {
-        return $this->render('article/show.html.twig');
+        // Get Repo
+        $repository = $em->getRepository(Article::class);
+
+        // Get article
+        /** @var Article $article */
+        $article = $repository->findOneBy(['slug' => $slug]);
+
+        if(!$article) {
+            throw $this->createNotFoundException(sprintf('Oops! No article, %s,found', $slug));
+        }
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
     }
 }
