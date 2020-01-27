2c307c2664f5b72df6adb0b347c1c6672c1b58b0
Andrew Adcock <andrewadcock@gmail.com>
2020-01-27 10:31:58 -0500

    Add first and last name fields

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -27,6 +27,16 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -96,5 +106,29 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
     }
 }
