3cb6c42d315c6c9b6a3df9bbdea07304ab947ae3
Andrew Adcock <andrewadcock@gmail.com>
2020-01-29 14:01:07 -0500

    Update widgets

---
+++ symfony/src/Controller/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Michelf\MarkdownInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+
+    /**
+     * @Route("/category/{slug}", name="category_show")
+     */
+    public function show($slug, EntityManagerInterface $em, MarkdownInterface $markdown)
+    {
+        // Get Repo
+        $repository = $em->getRepository(Category::class);
+
+        // Get category
+        /** @var Category $category */
+        $category = $repository->findOneBy(['slug' => $slug]);
+
+
+        if(!$category) {
+            throw $this->createNotFoundException(sprintf('Oops! No articles found in category: %s', $slug));
+        }
+
+        $articleRepo = $em->getRepository(Article::class);
+        $articles = $articleRepo->findByCategory($category->getId());
+
+        $archiveController = new ArticleController();
+        $archives = $archiveController->archivesByDateDesc($em);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'articles' => $articles,
+            'categories' => $this->categoriesAlpha($em),
+            'archives' => $archives,
+        ]);
+    }
+
+    public function categoriesAlpha(EntityManagerInterface $em) {
+        $repository = $em->getRepository(Category::class);
+        return $repository->findAllByNameOrderedAsc();
+
+    }
+
+}
